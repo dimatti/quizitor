d7881ca9aa95764df8c9a198ebba50da41d42646
dimatti <pffangv@gmail.com>
2020-05-18 01:54:55 +0300

    add gitignore

--- app/settings.py
+++ app/settings.py
@@ -23,10 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gx%zo43w4mxj1zz$zgza_b=o3##zx7xm(k17=v2maz$!jbc@xn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Application definition
 
