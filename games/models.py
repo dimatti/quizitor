ba1c81451afc63df7b090e300d15d50e169f8db7
dimatti <pffangv@gmail.com>
2020-06-05 02:28:12 +0300

    add models

--- games/models.py
+++ games/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 # Create your models here.
 
+
 class Point(models.Model):
     lat = models.FloatField(verbose_name=_('Point Latitude'))
     lon = models.FloatField(verbose_name=_('Point Longitude'))
@@ -9,6 +10,7 @@ class Point(models.Model):
     question = models.TextField(verbose_name=_('Question'), null=True)
     answer = models.TextField(verbose_name=_('Answer'), null=True)
     tip = models.TextField(verbose_name=_('Tip'), null=True)
+
 
 class Game(models.Model):
     name = models.TextField(verbose_name=_('Name'))
@@ -20,17 +22,15 @@ class Game(models.Model):
         ordering = ['-id']
 
 
-    def create_new(cls, name, points, questions, answers, tips):
-        """
-        name: string
-        points: list of tuples(lat, lon)
-        questions: list of str
-        answers: list of str
-        tips: list of str
-        """
-        if len(points) != len(questions) != len(answers) != len(tips):
-            raise Exception("create error")
-        points_lat, points_lon = []
-        for pair in points:
-            points_lat.append(pair[0])
-            points_lat.append(1)
+class Result(models.Model):
+    point = models.ForeignKey(Point, on_delete=models.CASCADE)
+    time_completed = models.DateTimeField(verbose_name=_('Time Completed'))
+    user_answer = models.TextField(verbose_name=_('User Answer'), null=True)
+    is_answered_correctly = models.BooleanField(verbose_name=_('Is Answered Correctly'), default=False)
+    is_checked_correctly = models.BooleanField(verbose_name=_('Is Checked Correctly'), default=False)
+
+
+class CurrentGame(models.Model):
+    source_game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    results = models.ManyToManyField(Result)
+    time_start = models.DateTimeField(verbose_name=_('Time Start'))
